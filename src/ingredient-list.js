885849256be8549cf87c84667fed567be5fc7425
Joe Turner <joe@oampo.co.uk>
2018-02-26 13:50:40 +0000

    Switch to stateful component

--- src/ingredient-list.js
+++ src/ingredient-list.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import Ingredient from './ingredient';
 
-export default function IngredientList() {
-    const ingredientData = [
-        {
-            name: 'Banana',
-            quantity: '10'
-        },
-        {
-            name: 'Chocolate Sauce',
-            quantity: '10 tbsp'
-        },
-        {
-            name: 'Chopped Hazelnuts',
-            quantity: '20'
-        }
-    ];
+export default class IngredientList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: [
+                {
+                    name: 'Banana',
+                    quantity: '10'
+                },
+                {
+                    name: 'Chocolate Sauce',
+                    quantity: '10 tbsp'
+                },
+                {
+                    name: 'Chopped Hazelnuts',
+                    quantity: '20'
+                }
+            ]
+        };
+    }
 
-    const ingredients = ingredientData.map((ingredient, index) => (
-        <Ingredient
-            name={ingredient.name}
-            quantity={ingredient.quantity}
-            key={index}
-        />
-    ));
-    return <div className="ingredient-list">{ingredients}</div>;
+    render() {
+        const ingredients = this.state.ingredients.map((ingredient, index) => (
+            <Ingredient
+                name={ingredient.name}
+                quantity={ingredient.quantity}
+                key={index}
+            />
+        ));
+        return <div className="ingredient-list">{ingredients}</div>;
+    }
 }
