5fdcc26a1d1784cabe30fdd832c2f75e236400a0
Joe Turner <joe@oampo.co.uk>
2018-02-26 13:49:22 +0000

    Map over ingredient data

--- src/ingredient-list.js
+++ src/ingredient-list.js
@@ -2,11 +2,27 @@ import React from 'react';
 import Ingredient from './ingredient';
 
 export default function IngredientList() {
-    return (
-        <div className="ingredient-list">
-            <Ingredient name="Banana" quantity="10" />
-            <Ingredient name="Chocolate Sauce" quantity="10 tbsp" />
-            <Ingredient name="Chopped hazelnuts" quantity="20" />
-        </div>
-    );
+    const ingredientData = [
+        {
+            name: 'Banana',
+            quantity: '10'
+        },
+        {
+            name: 'Chocolate Sauce',
+            quantity: '10 tbsp'
+        },
+        {
+            name: 'Chopped Hazelnuts',
+            quantity: '20'
+        }
+    ];
+
+    const ingredients = ingredientData.map((ingredient, index) => (
+        <Ingredient
+            name={ingredient.name}
+            quantity={ingredient.quantity}
+            key={index}
+        />
+    ));
+    return <div className="ingredient-list">{ingredients}</div>;
 }
